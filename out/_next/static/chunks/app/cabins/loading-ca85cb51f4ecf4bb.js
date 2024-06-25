54275a389055ed12a4e2355dc768f8cc500138e7
Aleksandar <aleksandar@e-vlasotince.info>
2024-06-25 15:31:04 +0200

    delete out folder

--- out/_next/static/chunks/app/cabins/loading-ca85cb51f4ecf4bb.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[404,555],{337:function(n,e,u){Promise.resolve().then(u.t.bind(u,4979,23))},4979:function(){}},function(n){n.O(0,[845,971,23,744],function(){return n(n.s=337)}),_N_E=n.O()}]);
